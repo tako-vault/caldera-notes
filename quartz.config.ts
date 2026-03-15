117d884bac7df1024a51480cb277576fa4e57249
monotako <lowe.buildscoolstuff@gmail.com>
2026-03-15 19:26:37 +0000

    Add deploy to github pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Notes on Caldera by Tiago",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
